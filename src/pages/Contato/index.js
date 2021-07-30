ed7c8540fb9f040ae1df7156d6daba0a33ea2437
Ingrid Miranda | Ambar <ingrid.miranda@ambar.tech>
2021-07-30 20:15:49 -0300

    implement contact page

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -2,9 +2,34 @@ import './styles.css'
 
 const Contato = ()=>{
     return (
-        <div>
-            Contato
-        </div>
+        <main className="container-contato">
+        
+            <h1>Dúvidas, sugestões ou reservas? Fale conosco!</h1>
+            <div>
+                <div className="container-first-line">
+                    <div>
+                        <h2>Nome</h2>
+                        <input text />
+                    </div>
+                    <div>
+                        <h2>Telefone</h2>
+                        <input text />
+                    </div>
+                </div>
+                <div>
+                    <h2>Email</h2>
+                    <input text />
+                </div>
+                <div>
+                    <h2>Assunto</h2>
+                    <input text />
+                </div>
+                <div>
+                    <h2>Mensagem</h2>
+                    <textarea text />
+                </div>
+            </div>            
+        </main>
     )
 }
 
